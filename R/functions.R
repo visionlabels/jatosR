b51e98c285f2b871f80649add7000da3f4ebeee8
Jiri Lukavsky <jirilukavsky@users.noreply.github.com>
2024-03-26 19:24:23 +0100

    Added raw_jspsych

--- R/functions.R
+++ R/functions.R
@@ -447,7 +447,7 @@ clean_demo <- function() {
 #' clean_demo()
 #' }
 raw_json <- function(d) {
-  stopifnot("Package jsonline must be installed" = requireNamespace("jsonlite", quietly = TRUE))
+  stopifnot("Package jsonlite must be installed" = requireNamespace("jsonlite", quietly = TRUE))
   if (tibble::is_tibble(d)) {
     result <-
       d |>
@@ -467,5 +467,65 @@ raw_json <- function(d) {
     result$data <- result$data |> raw_json()
     return(result)
   }
-
+  stop("Unsupported input type")
 }
+
+#' Transforms raw data produced by jsPsych to tibble
+#'
+#' Takes raw results stored in `data_raw` and adds new column `data_jspsych` containing data in lists created by `jspsychread` package.
+#' The packages `jsonlite` and `jspsychread` must be installed for this function to work.
+#' It is expected that the parameter `d` is the dataset obtained with `process_results`,
+#' but the dataset can be simpler, because only the column `data_raw` is used.
+#'
+#' Function `process_results` returns a list (let's assume called `results`),
+#' `raw_jspsych` is intended to be used in tibble pipeline, i.e. on the `result$data` tibble.
+#' To simplify the process, the function detects, whether it is provided
+#' with a list containing a tibble called `data`. In that case,
+#' it processes the `data` tibble, puts it back into the original list and returns it.
+#'
+#' @param d Tibble with data obtained with `process_results`
+#'
+#' @return Tibble with new list column `data_jspsych` or a list with `data` tibble (see above)
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' demo <- prepare_demo()
+#' r <- process_results(demo$meta, demo$results)
+#'
+#' # working with tibbles
+#' rdata <- r$data |> raw_jspsych()
+#' rdata |> select(start_date_sr, data_json)
+#' rdata$data_json[[1]]
+#'
+#' # shortcut for the results list also works
+#' r <- r |> raw_jspsych()
+#'
+#' clean_demo()
+#' }
+raw_jspsych <- function(d) {
+  stopifnot("Package jsonlite must be installed" = requireNamespace("jsonlite", quietly = TRUE))
+  stopifnot("Package jspsychread must be installed" = requireNamespace("jspsychread", quietly = TRUE))
+  if (tibble::is_tibble(d)) {
+    result <-
+      d |>
+      dplyr::mutate(
+        data_jspsych =
+          lapply(
+            .data[["data_raw"]],
+            function(x)
+              jspsychread::parse_single_record(jsonlite::parse_json(x))
+          )
+      )
+    return(result)
+  }
+  if (is.list(d) &&
+      !is.null(d[["data"]]) &&
+      tibble::is_tibble(d[["data"]])) {
+    result <- d
+    result$data <- result$data |> raw_jspsych()
+    return(result)
+  }
+  stop("Unsupported input type")
+}
+
